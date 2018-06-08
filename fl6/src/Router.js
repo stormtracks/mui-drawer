2452420317ee43d27e23da275295608af36163b6
Michael Angerman <stormasm@gmail.com>
2018-06-08 12:24:59 -0700

    fl6 is latest version of mui post 1.0 release as of june 8, 2018

---
+++ fl6/src/Router.js
@@ -0,0 +1,20 @@
+
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  withRouter
+} from 'react-router-dom'
+
+import Nav from './pages/Drawer4'
+
+const MyNav = withRouter(Nav);
+
+const NavTop = () => (
+  <Router>
+    <div>
+      <MyNav/>
+    </div>
+  </Router>
+)
+
+export default NavTop
